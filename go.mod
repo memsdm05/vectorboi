9dba44ace01b966d251cad0a00d2f44912eb3523
Ben Browner <bnbrowner@gmail.com>
2022-02-20 22:12:14 -0500

    new physics

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module vectorboi
 go 1.17
 
 require (
+	github.com/ByteArena/box2d v1.0.2
+	github.com/hajimehoshi/ebiten/v2 v2.2.5
+)
+
+require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- research/physics/main.go
+++ research/physics/main.go
@@ -3,40 +3,51 @@ package main
 import (
 	"github.com/ByteArena/box2d"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"vectorboi/helpers"
 )
 
 const PhysicsStep = 1. / 60
 
 type PhysicsTestGame struct {
-	world *box2d.B2World
+	world box2d.B2World
 	ground *box2d.B2Body
 	fallingBox *box2d.B2Body
 }
 
 func (p *PhysicsTestGame) Init()  {
-	p.world.M_gravity.Set(0, -0.001)
+	p.world = box2d.MakeB2World(box2d.MakeB2Vec2(0, -10))
 
-	body := box2d.NewB2BodyDef()
-	body.Type = box2d.B2BodyType.B2_dynamicBody
-	p.fallingBox = box2d.NewB2Body(body, p.world)
+	groundDef := box2d.NewB2BodyDef()
+	groundDef.Position.Set(0, -10)
+	groundBox := box2d.NewB2PolygonShape()
+	groundBox.SetAsBox(50, 10)
+	p.ground = box2d.NewB2Body(groundDef, &p.world)
+	p.ground.CreateFixture(groundBox, 0)
+
+	fallingBoxDef := box2d.NewB2BodyDef()
+	fallingBoxDef.Type = box2d.B2BodyType.B2_dynamicBody
+	fallingBoxDef.Position.Set(0, 4)
+	fallingBoxBox := box2d.NewB2PolygonShape()
+	fallingBoxBox.SetAsBox(1,1)
+	p.fallingBox = box2d.NewB2Body(fallingBoxDef, &p.world)
+	p.fallingBox.CreateFixture(fallingBoxBox, 1)
 }
 
-func (p *PhysicsTestGame) Shutdown()  {
-
-}
+func (p *PhysicsTestGame) Shutdown()  {}
 
 func (p *PhysicsTestGame) Update() error {
-	//p.world.Step()
+	p.world.Step(PhysicsStep, 10, 10)
 	return nil
 }
 
 func (p *PhysicsTestGame) Draw(screen *ebiten.Image) {
-	panic("implement me")
+	ebitenutil.DebugPrint(screen, "lol")
+	box := ebiten.NewImage(10, 10)
 }
 
 func (p *PhysicsTestGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 380
+	return 1280, 720
 }
 
 func main() {
